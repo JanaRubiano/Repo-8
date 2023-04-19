3625ce82b6a04bcf80ebc3247ce721d6291610e1
JanaRubiano <124604730+JanaRubiano@users.noreply.github.com>
2023-04-19 11:29:38 -0500

    Add files via upload

---
+++ point9.py
@@ -0,0 +1,25 @@
+'''
+The first input is a real number x, the second input is an integer n.
+x is the function's input and n the number of repetition the series will make.
+The outputs are: the value of sin(x) calcultaed by the compputer with the math module, 
+the Maclaurin series approximation of sin(x), the difference between both previous values, 
+and the number n that was needed to calculate the answer with less than a 0.1% error.
+'''
+import math
+def point9(x:float, n:int) -> float:
+    real = math.sin(x)
+    aprox = 0
+    error_limit = "Not enough to get an approximation under 0.1% of error"
+    for i in range(n+1):
+        aprox += ((-1)**i) * ((x**(2*i+1)) / (math.factorial(2*i+1)))
+        if round(aprox, 4) == round(real, 4):
+            error_limit = i
+            break
+    diff = real - aprox
+    return f"The real value is: {real}\nThe Maclaurin series approximation is: {aprox}\nThe difference between the real and the approximated value is: {diff}\nThe value for n when there is an error of less than 0.1% is: {error_limit}"
+
+if __name__ == "__main__":
+    x = float(input("Enter a real number: "))
+    n = int(input("Enter an integer: "))
+    res = point9(x, n)
+    print(res)
